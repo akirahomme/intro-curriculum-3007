c8a7b4e3be5aed7c15a127caee711bbfebe232ce
akirahomme <akira_20n1104182@nnn.ed.jp>
2020-07-16 08:53:11 +0900

    17で割り切れるかの判別

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
-module.exports = {
+
+/**17で割り切れるかの判別
+ *@param {number} num 
+ */
+function isMultipleOfSeventeen (num){
+    return num % 17 === 0;
 }
+
+
+module.exports = {
+    isMultipleOfSeventeen
+};
